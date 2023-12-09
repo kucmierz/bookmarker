f1eacc22b33bd4a1ce44c9484f00ba906b8d2805
kucmierz <marek.kucmierz@gmail.com>
2023-12-09 08:17:34 +0100

    a humble little window

---
+++ app/main.js
@@ -0,0 +1,8 @@
+const { app, BrowserWindow } = require("electron");
+
+let mainWindow = null;
+
+app.on("ready", () => {
+  console.log("Hello from Electron");
+  mainWindow = new BrowserWindow();
+});
